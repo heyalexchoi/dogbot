fae35acb194d240f578c57a638b098d53c0fe7b5
heyalexchoi <heyalexchoi@gmail.com>
2015-12-16 11:47:00 -0500

    can use icon url now. esp rubby's lol

--- app/services/dogbot.rb
+++ app/services/dogbot.rb
@@ -36,8 +36,8 @@ class Dogbot
     end
 
     # search channels for id for name?
-    def say(to:, from: name, text: , link_names: true)
-      client.chat_postMessage({channel: to, text: text, username: from, link_names: true})
+    def say(to:, from: name, text: , link_names: true, as_user: false, icon_url: nil)
+      client.chat_postMessage({channel: to, text: text, username: from, link_names: true, as_user: as_user, icon_url: icon_url})
     end
 
     def tell_tmnt(text)
@@ -61,18 +61,22 @@ class Dogbot
       urls = Giphy.search(term).map { |g| g.original_image.url.to_s }
     end
 
-    def send_gif(to: , from: name, text: , gif_term: , link_names: true)
+    def send_gif(to: , from: name, text: , gif_term: , link_names: true, as_user: false, icon_url: nil)
       gif_url = gif(gif_term)
       message_text = "<#{gif_url} | #{text}>"
-      say(to: to, from: from, link_names: link_names, text: message_text)
+      say(to: to, from: from, link_names: link_names, text: message_text, as_user: as_user, icon_url: icon_url)
     end
 
-    def send_gifs(to: , from: name, text: , gif_term:, link_names: true)
+    def send_gifs(to: , from: name, text: , gif_term:, link_names: true, as_user: false, icon_url: nil)
       gifs = self.gifs(gif_term)
-      say(to: to, from: from, text: text)
+      say(to: to, from: from, text: text, as_user: as_user, icon_url: icon_url)
       gifs.map do |g|
-        say(to: to, from: from, text: "<#{g}| >")
+        say(to: to, from: from, text: "<#{g}| >", as_user: as_user, icon_url: icon_url)
       end
+    end
+
+    def ruby_icon_url
+      'https://pbs.twimg.com/profile_images/449242540110139392/D4JO7BZs.jpeg'
     end
 
   end
